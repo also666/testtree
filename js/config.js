8b68ced1c4c06789454adeecbb20e84f42c7acef
also666 <l>
2019-01-05 00:56:39 +0800

    first commit

---
+++ js/config.js
@@ -0,0 +1,1 @@
+var devUrl="https://test.orex.one";
---
+++ js/data.js
@@ -0,0 +1,192 @@
+var postData = {
+			    "status": "ok",
+			    "message": "获取社交圈成功！",
+			    "data": {
+			        "fatherOrder": 1,
+			        "processedData": {
+			            "1": 16,
+			            "2": 169,
+			            "3": 170,
+			            "4": 172,
+			            "5": 171,
+			            "6": 174,
+			            "7": 173,
+			            "8": 175,
+			            "9": 177,
+			            "10": 179,
+			            "11": 182,
+			            "12": 176,
+			            "13": 180,
+			            "14": 178,
+			            "20": 181
+			        },
+			        "pendingData": [],
+			        "userResult": [
+			            {
+			                "id": 16,
+			                "mobile": "15002137023",
+			                "pin": "0001",
+			                "master": 1,
+			                "left_apprentice": 169,
+			                "right_apprentice": 170,
+			                "left_length": 3,
+			                "right_length": 3,
+			                "invited_num": 9
+			            },
+			            {
+			                "id": 169,
+			                "mobile": "15002137023",
+			                "pin": "1010",
+			                "master": 16,
+			                "left_apprentice": 172,
+			                "right_apprentice": 171,
+			                "left_length": 2,
+			                "right_length": 2,
+			                "invited_num": 2
+			            },
+			            {
+			                "id": 170,
+			                "mobile": "15002137023",
+			                "pin": "1030",
+			                "master": 16,
+			                "left_apprentice": 174,
+			                "right_apprentice": 173,
+			                "left_length": 2,
+			                "right_length": 2,
+			                "invited_num": 2
+			            },
+			            {
+			                "id": 171,
+			                "mobile": "16602196290",
+			                "pin": "7777",
+			                "master": 169,
+			                "left_apprentice": 179,
+			                "right_apprentice": 182,
+			                "left_length": 1,
+			                "right_length": 1,
+			                "invited_num": 2
+			            },
+			            {
+			                "id": 172,
+			                "mobile": "17612188897",
+			                "pin": "1111",
+			                "master": 169,
+			                "left_apprentice": 175,
+			                "right_apprentice": 177,
+			                "left_length": 1,
+			                "right_length": 1,
+			                "invited_num": 2
+			            },
+			            {
+			                "id": 173,
+			                "mobile": "13120627899",
+			                "pin": "0007",
+			                "master": 170,
+			                "left_apprentice": 178,
+			                "right_apprentice": 181,
+			                "left_length": 1,
+			                "right_length": 1,
+			                "invited_num": 2
+			            },
+			            {
+			                "id": 174,
+			                "mobile": "17521137007",
+			                "pin": "5001",
+			                "master": 170,
+			                "left_apprentice": 176,
+			                "right_apprentice": 180,
+			                "left_length": 1,
+			                "right_length": 1,
+			                "invited_num": 2
+			            },
+			            {
+			                "id": 175,
+			                "mobile": "17612188897",
+			                "pin": "2222",
+			                "master": 172,
+			                "left_apprentice": null,
+			                "right_apprentice": null,
+			                "left_length": 0,
+			                "right_length": 0,
+			                "invited_num": 0
+			            },
+			            {
+			                "id": 176,
+			                "mobile": "17521137007",
+			                "pin": "5002",
+			                "master": 174,
+			                "left_apprentice": null,
+			                "right_apprentice": null,
+			                "left_length": 0,
+			                "right_length": 0,
+			                "invited_num": 0
+			            },
+			            {
+			                "id": 177,
+			                "mobile": "17612188897",
+			                "pin": "3333",
+			                "master": 172,
+			                "left_apprentice": null,
+			                "right_apprentice": null,
+			                "left_length": 0,
+			                "right_length": 0,
+			                "invited_num": 0
+			            },
+			            {
+			                "id": 178,
+			                "mobile": "13120627899",
+			                "pin": "0008",
+			                "master": 173,
+			                "left_apprentice": null,
+			                "right_apprentice": null,
+			                "left_length": 0,
+			                "right_length": 0,
+			                "invited_num": 0
+			            },
+			            {
+			                "id": 179,
+			                "mobile": "16602196290",
+			                "pin": "8888",
+			                "master": 171,
+			                "left_apprentice": null,
+			                "right_apprentice": null,
+			                "left_length": 0,
+			                "right_length": 0,
+			                "invited_num": 0
+			            },
+			            {
+			                "id": 180,
+			                "mobile": "17521137007",
+			                "pin": "5003",
+			                "master": 174,
+			                "left_apprentice": null,
+			                "right_apprentice": null,
+			                "left_length": 0,
+			                "right_length": 0,
+			                "invited_num": 0
+			            },
+			            {
+			                "id": 181,
+			                "mobile": "13120627899",
+			                "pin": "0009",
+			                "master": 173,
+			                "left_apprentice": null,
+			                "right_apprentice": null,
+			                "left_length": 0,
+			                "right_length": 0,
+			                "invited_num": 0
+			            },
+			            {
+			                "id": 182,
+			                "mobile": "16602196290",
+			                "pin": "9999",
+			                "master": 171,
+			                "left_apprentice": null,
+			                "right_apprentice": null,
+			                "left_length": 0,
+			                "right_length": 0,
+			                "invited_num": 1
+			            }
+			        ]
+			    }
+			};
